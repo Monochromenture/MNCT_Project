21ae4ec170abfc25079a15ed4fb88b14d54f5563
tyl4876 <tsuki0515@gmail.com>
2025-02-16 01:27:15 +0800

    0216-2
    修改玩家移動細節

--- Assets/Scripts/ButtonCrl.cs
+++ Assets/Scripts/ButtonCrl.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ButtonCrl : MonoBehaviour
 {
+
+
+    public void QuitGame()
+    {
+        Application.Quit(); // 關閉遊戲（適用於 Build 版本）
+        Debug.Log("Game is quitting..."); // 在 Unity Editor 內部測試
+    }
+
     // Start is called before the first frame update
     void Start()
     {
